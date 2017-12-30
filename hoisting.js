5e61359073db23f79062b786224b89bb1d9a6b6f
SayeedM <dmbterminal@gmail.com>
2017-12-31 03:08:57 +0600

    updated hoisting.js

--- hoisting.js
+++ hoisting.js
@@ -1,4 +1,3 @@
-/* from you don't know js - up and going */
 var a = 2;
 
 /* foo is called before its declaration - function hoisting */
@@ -12,4 +11,63 @@ function foo(){
 }
 /* now we get back global scope a which contains 2 */
 console.log("a = " + a);
+
+
+/* what about hoisiting with declarations */
+
+/* 
+   should print "undefined" as the following codes are breaked in to this :
+   var aa;
+   console.log(aa);
+   aa = 2;
+*/
+console.log(aa);
+var aa = 2; 
+
+/* function declarations are hoisted - not expression */
+
+/* 
+    should get type error as it is translated to following : 
+    var functionExpression;
+    functionExpression(); //--> undefined();
+    functionExpression = function(){
+	console.log("function expression");
+    }
+*/
+//functionExpression(); // commented for further compilation
+var functionExpression = function(){
+	console.log("function expression");
+}
+
+/* functions are hoisted first, then the variables */
+test();
+var test;
+
+function test(){
+	console.log("test function first hoisiting");
+}
+
+//or test = some value;
+test = function(){
+	console.log("this won't be printed, the function was hoisted first to name test");
+}
+
+
+/* 
+  if there are two functions with same name (not function expressions assigned to varable)
+  the second one is hoisted
+*/
+testFunctionHoisting();
+
+function testFunctionHoisting(){
+	console.log(1);
+}
+
+function testFunctionHoisting(){
+	console.log(2);
+}
+
+
+
+
 
